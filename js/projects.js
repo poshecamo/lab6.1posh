3e56c4268d3da9597d518874cf1356a8e2b60214
Polina <poshecamo@gmail.com>
2024-08-21 20:25:54 -0700

    updated projects js code

--- js/projects.js
+++ js/projects.js
@@ -1,18 +1,23 @@
-//prompt the user for a new name project 1
-let i =1;
-let project1 = prompt(`'enter name for a project'${i}:` );
-document.getElementById('proj1').textContent = project1;
+let i = 1;
+let project1 = prompt("Enter a name for Project ${i} :");
+document.getElementById("proj1").textContent = project1;
 
-i = i=1;
-document.getElementById('proj2').textContent = project2;
+// Prompt the user for a new name project 2
+i = i + 1;
+let project2 = prompt("Enter a name for Project ${i} :");
+document.getElementById("proj2").textContent = project2;
 
-i = i=1;
-document.getElementById('proj3').textContent = project3;
+// Prompt the user for a new name project 3
+i = i + 1;
+let project3 = prompt("Enter a name for Project ${i} :");
+document.getElementById("proj3").textContent = project3;
 
-i = i=1;
-document.getElementById('proj3').textContent = project3;
+// Prompt the user for a new name project 4
+i = i + 1;
+let project4 = prompt("Enter a name for Project ${i} :");
+document.getElementById("proj4").textContent = project4;
 
-
-
-
-
+// Prompt the user for a new name project 5
+i = i + 1;
+let project5 = prompt("Enter a name for Project ${i} :");
+document.getElementById("proj5").textContent = project5;
