e5ba937413dbab6b15bbe847998f705c9a564fa0
Polina <poshecamo@gmail.com>
2024-08-21 17:53:10 -0700

    updated projects.js projects.html

---
+++ js/projects.js
@@ -0,0 +1,18 @@
+//prompt the user for a new name project 1
+let i =1;
+let project1 = prompt(`'enter name for a project'${i}:` );
+document.getElementById('proj1').textContent = project1;
+
+i = i=1;
+document.getElementById('proj2').textContent = project2;
+
+i = i=1;
+document.getElementById('proj3').textContent = project3;
+
+i = i=1;
+document.getElementById('proj3').textContent = project3;
+
+
+
+
+
